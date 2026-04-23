9c35c014d8f0932daab3660c549ae23487c88812
ikaczuk <ika.walczuk@gmail.com>
2026-04-23 23:20:52 +0200

    SQLite added, register and login pages are working now

--- app/src/main/java/com/example/loginsignupsql_flow/LoginActivity.kt
+++ app/src/main/java/com/example/loginsignupsql_flow/LoginActivity.kt
@@ -1,20 +1,48 @@
 package com.example.loginsignupsql_flow
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.loginsignupsql_flow.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var databaseHelper: DatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        databaseHelper = DatabaseHelper(this)
+
+        binding.loginButton.setOnClickListener {
+            val loginUsername = binding.loginUsername.text.toString()
+            val loginPassword = binding.loginPassword.text.toString()
+            loginDatabase(loginUsername, loginPassword)
+        }
+
+        binding.signupRedirect.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+    private fun loginDatabase(username: String, password: String) {
+        val userExists = databaseHelper.readUser(username, password)
+        if (userExists) {
+            Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        else {
+            Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/example/loginsignupsql_flow/SignupActivity.kt
+++ app/src/main/java/com/example/loginsignupsql_flow/SignupActivity.kt
@@ -1,20 +1,49 @@
 package com.example.loginsignupsql_flow
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.loginsignupsql_flow.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
+    private lateinit var databaseHelper: DatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        databaseHelper = DatabaseHelper(this)
+
+        binding.signupButton.setOnClickListener {
+            val signupUsername = binding.signupUsername.text.toString()
+            val signupPassword = binding.signupPassword.text.toString()
+            signupDatabase(signupUsername, signupPassword)
+        }
+
+        binding.loginRedirect.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    private fun signupDatabase(username: String, password: String) {
+        val insertedRowId = databaseHelper.insertUser(username, password)
+        if(insertedRowId != -1L) {
+            Toast.makeText(this, "Signup Successful", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        else {
+            Toast.makeText(this, "signul Failed", Toast.LENGTH_SHORT).show()
         }
     }
 }
