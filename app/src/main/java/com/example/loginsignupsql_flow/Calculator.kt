cca3e8700db3730490a06ce3bf1a12b197ac54cf
ikaczuk <ika.walczuk@gmail.com>
2026-04-25 15:14:44 +0200

    made a working menu. When clicked on an icon the page changes into the clicked one

---
+++ app/src/main/java/com/example/loginsignupsql_flow/Calculator.kt
@@ -0,0 +1,21 @@
+package com.example.loginsignupsql_flow
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class Calculator : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calculator, container, false)
+    }
+}
--- app/src/main/java/com/example/loginsignupsql_flow/MainActivity.kt
+++ app/src/main/java/com/example/loginsignupsql_flow/MainActivity.kt
@@ -1,20 +1,106 @@
 package com.example.loginsignupsql_flow
 
+import android.R.id.home
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var toolbar: Toolbar
+
+    @Override
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+
+        navigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        toggle.drawerArrowDrawable.color = ContextCompat.getColor(this, R.color.blue)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.framelayout, Home())
+                .commit()
+
+            navigationView.setCheckedItem(R.id.home)
         }
+    }
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.home -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.framelayout, Home())
+                    .commit()
+            }
+            R.id.account -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.framelayout, Account())
+                    .commit()
+            }
+            R.id.calculator -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.framelayout, Calculator())
+                    .commit()
+            }
+            R.id.daily -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.framelayout, Daily_intake())
+                    .commit()
+            }
+            R.id.history -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.framelayout, History())
+                    .commit()
+            }
+            R.id.notifications -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.framelayout, Notifications())
+                    .commit()
+            }
+            R.id.settings -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.framelayout, Settings())
+                    .commit()
+            }
+            R.id.logout -> {
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START)
+
+        fun onBackPressed() {
+            if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                super.onBackPressed()
+            }
+        }
+
+        return true
     }
 }
